d7762953921c4e0fb7c00e929ec494d67bcd457c
lahirumaya12 <clahiru789@gmail.com>
2023-07-03 10:22:20 +0530

    Fixed apiclient issue

--- src/RestApi/apiClient.js
+++ src/RestApi/apiClient.js
@@ -1,23 +1,18 @@
 import axios from "axios";
-import { getCities } from '../RestApi/getCities';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-
-
-const cityCodes = getCities;
 
 const apiClient = axios.create({
   baseURL: 'http://api.openweathermap.org/data/2.5',
 });
 
-export const getWeatherData = async () => {
-    try {
-      
-      const cityIds = cityCodes.join(',');
-      const response = await apiClient.get(`/group?id=${cityIds}&units=metric&appid=${API_KEY}`);
-      return response.data.list;
-    } catch (error) {
-      throw new Error(`Error fetching cities: ${error.message}`);
-    }
-  };
+export const getWeatherData = async (cityCodes) => {
+  try {
+    const response = await apiClient.get(`/group?id=${cityCodes}&units=metric&appid=${API_KEY}`);
+    return response.data.list;
+  } catch (error) {
+    throw new Error(`Error fetching weather data: ${error.message}`);
+  }
+};
+
 
